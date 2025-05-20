03f070c9f3e7d0537d5fe401316fa5d50a6dd763
Quang Trong <quangtrong1506@gmail.com>
2025-05-21 00:26:18 +0700

    "test-"

--- renderer/components/shortcut/item/shortcut-item.tsx
+++ renderer/components/shortcut/item/shortcut-item.tsx
@@ -3,26 +3,44 @@ import { ShortcutInterface } from './type';
 import { ImageContainer } from '../../image-container';
 
 export interface ShortcutItemProps {
-    className?: string;
-    item: ShortcutInterface;
+  className?: string;
+  item: ShortcutInterface;
 }
+
+const fontSize = {
+  1: '10px',
+  2: '12px',
+  3: '16px',
+  4: '18px',
+  5: '20px',
+  6: '22px',
+};
 
 /**
  * Shortcut Item với hỗ trợ ref
  */
 export const ShortcutItem = forwardRef<HTMLDivElement, Readonly<ShortcutItemProps>>(function ShortcutItem(props, ref) {
-    const { className = '', item } = props;
-    const scale = [1, 2, 3, 4, 5].find((s) => item.scale === s) || 1;
+  const { className = '', item } = props;
+  console.log(item.scale);
 
-    return (
-        <div
-            ref={ref}
-            className={`w-full h-full rounded-xl select-none hover:bg-white/5 px-3 dark:hover:bg-black/5 ${className}`}
-        >
-            <div className="flex justify-center aspect-square overflow-hidden items-center">
-                <ImageContainer src={item.icon} alt={item.title} />
-            </div>
-            <div className="line-clamp-2 text-center">{item.title}</div>
-        </div>
-    );
+  const scale = [1, 2, 3, 4, 5, 6].find((s) => item.scale === s) || 1;
+
+  return (
+    <div
+      ref={ref}
+      className={`w-full h-full flex py-2 items-center flex-col rounded-md select-none hover:bg-white/5 px-3 dark:hover:bg-black/5 ${className}`}
+    >
+      <div className="flex w-3/4 justify-center aspect-square overflow-hidden items-center">
+        <ImageContainer src={item.icon} alt={item.title} />
+      </div>
+      <div
+        className="line-clamp-2 text-center"
+        style={{
+          fontSize: fontSize[scale],
+        }}
+      >
+        {item.title}
+      </div>
+    </div>
+  );
 });
